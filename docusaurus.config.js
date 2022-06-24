7a64f4c3577364640ee94ef438abceb7149c12f9
SengMitnick <sengmitnick@163.com>
2022-06-24 14:56:12 +0800

    adjust: 去掉Edit this page；使用仅文档模式；去掉顶部的开始指南和GitHub标志

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,33 +1,35 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '1024Code 文档中心',
-  tagline: '1024Code',
-  url: 'https://1024-docs.netlify.app',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/logo-transparent.png',
-  organizationName: 'dao42', // Usually your GitHub org/user name.
-  projectName: '1024_docs', // Usually your repo name.
+  title: "1024Code 文档中心",
+  tagline: "1024Code",
+  url: "https://1024-docs.netlify.app",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/logo-transparent.png",
+  organizationName: "dao42", // Usually your GitHub org/user name.
+  projectName: "1024_docs", // Usually your repo name.
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        blog: false,
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/dao42/1024_docs/edit/main',
+          // editUrl: "https://github.com/dao42/1024_docs/edit/main",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -37,27 +39,27 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: '1024Code 文档中心',
+        title: "1024Code 文档中心",
         logo: {
-          alt: '1024Code 文档中心',
-          src: 'img/logo-transparent.png',
+          alt: "1024Code 文档中心",
+          src: "img/logo-transparent.png",
         },
         items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: '开始指南',
-          },
-          {
-            href: 'https://github.com/dao42/1024_docs',
-            label: 'GitHub',
-            position: 'right',
-          },
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: '开始指南',
+          // },
+          // {
+          //   href: 'https://github.com/dao42/1024_docs',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} 1024Code`,
       },
       prism: {
